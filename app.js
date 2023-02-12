b8ef7a48fbedd4fe4a0e2967f239ac14a22675ef
Mike Heavers <mheavers@gmail.com>
2023-02-11 19:01:32 -0800

    final output

--- app.js
+++ app.js
@@ -2,9 +2,24 @@
 //const vision = require("@google-cloud/vision");
 
 import vision from "@google-cloud/vision";
+import fs from "fs";
 
 // Creates a client
 const client = new vision.ImageAnnotatorClient();
+
+// const scores = [
+//   "https://www.indicereparabilite.fr/wp-content/uploads/2021/08/0-a-19-3-230x350.png",
+//   "https://www.indicereparabilite.fr/wp-content/uploads/2021/08/6-a-79-46-230x350.png",
+//   "https://www.indicereparabilite.fr/wp-content/uploads/2021/08/4-a-59-35-230x350.png",
+//   "https://www.indicereparabilite.fr/wp-content/uploads/2021/08/4-a-59-34-230x350.png",
+//   "https://www.indicereparabilite.fr/wp-content/uploads/2021/08/6-a-79-45-230x350.png",
+//   "https://www.indicereparabilite.fr/wp-content/uploads/2021/08/6-a-79-43-230x350.png",
+//   "https://www.indicereparabilite.fr/wp-content/uploads/2021/08/4-a-59-33-230x350.png",
+// ];
+
+// const scores = require("./repairability-images.json");
+//convert scores require to an es module import {  } from "module";
+import scores from "./repairability-images.json" assert { type: "json" };
 
 /**
  * TODO(developer): Uncomment the following lines before running the sample.
@@ -13,6 +28,31 @@ const client = new vision.ImageAnnotatorClient();
 // const fileName = 'Path to file within bucket, e.g. path/to/image.png';
 
 // Performs text detection on the gcs file
+
+const writeLine = function (data) {
+  fs.appendFile(
+    "./repairability-scores.txt",
+    data.join("\r\n"),
+    "utf8",
+    (err) => {
+      if (err) {
+        console.log(`Error writing to file: ${err}`);
+      }
+    }
+  );
+};
+
+const writeAllData = function (data) {
+  const dataJSON = JSON.stringify(data);
+
+  fs.writeFile("./repairability-scores.json", dataJSON, "utf8", (err) => {
+    if (err) {
+      console.log(`Error writing file: ${err}`);
+    } else {
+      console.log(`File is written successfully!`);
+    }
+  });
+};
 
 async function getSingleScore(url) {
   const [result] = await client.textDetection(url);
@@ -29,86 +69,26 @@ async function getSingleScore(url) {
   }
 }
 
-/*
-const [result] = await client.textDetection(
-  `https://www.indicereparabilite.fr/wp-content/uploads/2021/08/4-a-59-35-230x350.png`
-);
-const detections = result.textAnnotations;
-console.log("Text:");
-const score = parseFloat(
-  detections[0].description.slice(0, 3).replace(",", ".")
-);
-console.log(score);
-*/
-
-/*
-detections.forEach((text) => {
-  console.log(text));
-}
-*/
-
-/*
-const score = await getSingleScore(
-  "https://www.indicereparabilite.fr/wp-content/uploads/2021/08/4-a-59-35-230x350.png"
-);*/
-
-const scores = [
-  "https://www.indicereparabilite.fr/wp-content/uploads/2021/08/0-a-19-3-230x350.png",
-  "https://www.indicereparabilite.fr/wp-content/uploads/2021/08/6-a-79-46-230x350.png",
-  "https://www.indicereparabilite.fr/wp-content/uploads/2021/08/4-a-59-35-230x350.png",
-  "https://www.indicereparabilite.fr/wp-content/uploads/2021/08/4-a-59-34-230x350.png",
-  "https://www.indicereparabilite.fr/wp-content/uploads/2021/08/6-a-79-45-230x350.png",
-  "https://www.indicereparabilite.fr/wp-content/uploads/2021/08/6-a-79-43-230x350.png",
-  "https://www.indicereparabilite.fr/wp-content/uploads/2021/08/4-a-59-33-230x350.png",
-];
-
 const scoreResults = [];
 
 async function init() {
-  /*
-  scores.forEach(async (score) => {
-    const result = await getSingleScore(score);
-    scoreResults.push(result);
-  });
-  */
-
-  /*
-  for (const score of scores) {
-    const result = await getSingleScore(score);
-    scoreResults.push(result);
-  }
-  */
-
-  /*
-
-  const scoresAll = await Promise.all(
-    scores.map(async (score) => {
-      score = await getSingleScore(score);
-      if (score) {
-        scoreResults.push(score);
-        return score;
-      }
-    })
-  ).then((values) => {
-    //console.log(values);
-    console.log(scoreResults);
-  });
-
-  */
-
-  const scoresAll = await scores.reduce(async (a, score) => {
+  await scores.reduce(async (a, score) => {
     // Wait for the previous item to finish processing
     await a;
     // Process this item
     score = await getSingleScore(score);
+    //write each score to a new line of a text file with node fs
+
     if (score) {
       scoreResults.push(score);
     } else {
       scoreResults.push(-1);
     }
   }, Promise.resolve());
+
   console.log("scoreResults");
   console.log(scoreResults);
+  writeAllData(scoreResults);
 }
 
 init();
